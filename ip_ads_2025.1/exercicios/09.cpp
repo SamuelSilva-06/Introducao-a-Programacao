f5d80978ea29ec9b93fae428d28d5ffa1956fad1
Samuel Silva <fcosamuel11@gmail.com>
2025-07-02 13:39:44 -0300

    FIZ a atvd 09

--- ip_ads_2025.1/exercicios/09.cpp
+++ ip_ads_2025.1/exercicios/09.cpp
@@ -4,3 +4,33 @@ P = 2(l + c)
 A = lc
 onde 'l' é a largura e 'c' é o comprimento.
 */
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+double perimetro(double& larg , double& comp){
+    return 2 * (larg + comp);
+}
+
+double area(double& larg , double& comp){
+    return larg * comp;
+}
+
+
+
+int main(){
+    double larg,comp;
+    printf("Digite a largura e o comprimento de um retangulo\n");
+    cin >> larg >> comp ;
+
+    double p = perimetro(larg,comp);
+    double a = area(larg,comp);
+    printf("O perimetro do retangulo e: %.1f\n",p);
+
+    printf("A area do retangulo e: %.1f",a);
+
+    return 0;
+    
+
+}
