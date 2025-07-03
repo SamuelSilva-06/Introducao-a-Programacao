50116e88945ba1ec7fd0264fc91697adfbdaa3d1
Samuel Silva <fcosamuel11@gmail.com>
2025-07-03 15:52:38 -0300

    fiz o exercicio 11

--- ip_ads_2025.1/exercicios/11.cpp
+++ ip_ads_2025.1/exercicios/11.cpp
@@ -3,3 +3,21 @@ Escreva um programa que calcule a velocidade média de um objeto, utilizando a f
 v = ds/dt
 onde 'v' é a velocidade média, 'ds' é a variação de espaço e 'dt' variação de tempo.
 */
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+double velocidadem(double& distancia , double& tempo){
+    return distancia / tempo;
+}
+
+int main(){
+    double distancia , tempo;
+    printf("Digite a distancia do objeto em km/h e o tempo gasto em horas.\n");
+    cin >> distancia >> tempo;
+    double velocidademedia = velocidadem(distancia,tempo);
+
+    printf("A velocidade media de um objeto e: %.1f km/h",velocidademedia);
+
+}
