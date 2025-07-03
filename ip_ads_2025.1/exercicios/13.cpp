d4e4359b847b0990b6e125eebe34dd18222e3831
Samuel Silva <fcosamuel11@gmail.com>
2025-07-03 15:58:55 -0300

    Fiz o exercicio 13

--- ip_ads_2025.1/exercicios/13.cpp
+++ ip_ads_2025.1/exercicios/13.cpp
@@ -3,3 +3,16 @@ Escreva um programa que calcule o trabalho realizado por uma força que atua sob
 T = F*d
 onde 'T' é o trabalho, 'F' é a força aplicada e 'd' é a distância percorrida pelo objeto.
 */
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    double forca, distancia;
+    cin >> forca >> distancia;
+
+    double trabalho = forca * distancia;
+    printf("%.1f",trabalho);
+
+}
